1fc732a34cad2ff4d2b34ea5c8faadf721ad6229
shifttag <ehdgus0837@naver.com>
2024-10-14 16:26:37 +0900

    20241014

---
+++ spirngboot-developer/src/main/java/org/example/이론/c_단축키.java
@@ -0,0 +1,16 @@
+package org.example.이론;
+
+public class c_단축키 {
+    
+    // 인텔리제이 단축키 (공용 단축키 포함)
+    
+    // - 줄 복사: ctrl + d    / MacOS) Cmd + d
+    // - 실행: shift + f10    / MacOs) ^ + r
+    // - 사용하지 않는 import문 삭제: ctrl + alt + o     / MacOs) ^ + option + o
+    
+    // 인텔리제이 단축키
+    // - 통합 검색: shift * 2
+    // - 열려 있는 파일 리스트: ctrl + e     / MacOs) Cmd + e
+    // - 줄 이동 alt + shift + 위/아래 방향키
+    
+}
