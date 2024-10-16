7489a772ec3a17e8519bded20e1a23d30c518096
shifttag <ehdgus0837@naver.com>
2024-10-16 12:33:44 +0900

    20241016

---
+++ spirngboot-developer/src/test/java/AssertJTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+// gradle에서는 기본적으로 src/test/java 폴더를 test파일의 소스경로로 인식
+// : java 폴더 내의 하위 테스트 클래스에는 static import를 사용하여
+// : Assertion의 메서드를 직접 호출
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AssertJTest {
+    @Test
+    public void assertJTest() {
+        String name1 = "홍동현";
+        String name2 = "홍길동";
+        String name3 = "김명진";
+
+        // 모든 변수가 null이 아닌지 확인
+        assertThat(name1).isNotNull();
+        assertThat(name2).isNotNull();
+        assertThat(name3).isNotNull();
+
+        // name1과 name2가 같은지 확인
+//        assertThat(name1).isEqualTo(name2);
+
+        // name1과 name2가 다른지 확인
+//        assertThat(name1).isNotEqualTo(name2);
+
+
+    }
+}
