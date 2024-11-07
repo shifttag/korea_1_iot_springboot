1ab043717740154fc413535a64e5e141e78302f7
shifttag <ehdgus0837@naver.com>
2024-11-07 09:15:26 +0900

    20241107

--- spirngboot-developer/src/main/java/org/example/springbootdeveloper/controller/PostController.java
+++ spirngboot-developer/src/main/java/org/example/springbootdeveloper/controller/PostController.java
@@ -1,16 +1,20 @@
 package org.example.springbootdeveloper.controller;
 
+import org.example.springbootdeveloper.common.constant.ApiMappingPattern;
 import org.example.springbootdeveloper.dto.request.PostRequestDto;
+import org.example.springbootdeveloper.dto.response.PagedResponseDto;
 import org.example.springbootdeveloper.dto.response.PostResponseDto;
 import org.example.springbootdeveloper.dto.response.ResponseDto;
 import org.example.springbootdeveloper.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping(ApiMappingPattern.POST)
 public class PostController {
 
     @Autowired
@@ -25,9 +29,25 @@ public class PostController {
     }
 
     // 2. 조회
+//    @GetMapping
+//    public ResponseDto<List<PostResponseDto>> getAllPosts() {
+//        return postService.getAllPosts();
+//    }
+
     @GetMapping
-    public ResponseDto<List<PostResponseDto>> getAllPosts() {
-        return postService.getAllPosts();
+    public ResponseEntity<ResponseDto<PagedResponseDto<PostResponseDto>>> getPosts(
+        // @RequestParam: upl을 통해 key와 value 형태로 요청값을 보냄.
+        // page: 현재 페이지 번호, size: 페이지 당 표시할 데이터 개수
+        @RequestParam int page,
+        @RequestParam int size) {
+        try {
+            // 요청한 페이지에 대한 데이터를 반환(응답)
+            ResponseDto<PagedResponseDto<PostResponseDto>> result = postService.getPosts(page, size);
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping("/{postId}")
--- spirngboot-developer/src/main/java/org/example/springbootdeveloper/service/PostService.java
+++ spirngboot-developer/src/main/java/org/example/springbootdeveloper/service/PostService.java
@@ -1,7 +1,9 @@
 package org.example.springbootdeveloper.service;
 
+import org.example.springbootdeveloper.common.constant.ResponseMessage;
 import org.example.springbootdeveloper.dto.request.PostRequestDto;
 import org.example.springbootdeveloper.dto.response.CommentResponseDto;
+import org.example.springbootdeveloper.dto.response.PagedResponseDto;
 import org.example.springbootdeveloper.dto.response.PostResponseDto;
 import org.example.springbootdeveloper.dto.response.ResponseDto;
 import org.example.springbootdeveloper.entity.Comment;
@@ -9,6 +11,8 @@ import org.example.springbootdeveloper.entity.Post;
 import org.example.springbootdeveloper.repository.CommentRepository;
 import org.example.springbootdeveloper.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -23,9 +27,6 @@ public class PostService {
     @Autowired
     private CommentRepository commentRepository;
 
-    public PostService(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
 
     // 1. 추가
     public ResponseDto<PostResponseDto> createPost(PostRequestDto dto) {
@@ -39,28 +40,75 @@ public class PostService {
 
     }
 
-
-
-
-
     // 2. 조회
-    public ResponseDto<List<PostResponseDto>> getAllPosts() {
-        try{
-            List<Post> posts = postRepository.findAll();
-            List<PostResponseDto> postResponseDto = posts.stream()
-                    .map(this::convertToPostDto)
+//    public ResponseDto<List<PostResponseDto>> getAllPosts() {
+//        try{
+//            List<Post> posts = postRepository.findAll();
+//            List<PostResponseDto> postResponseDto = posts.stream()
+//                    .map(this::convertToPostDto)
+//                    .collect(Collectors.toList());
+//
+//            if(postResponseDto.isEmpty()) {
+//                return ResponseDto.setFailed("등록된 게시글이 없습니다.");
+//            }
+//            return ResponseDto.setSuccess("게시글을 성공적으로 조회되었습니다.", postResponseDto);
+//        } catch (Exception e) {
+//            return ResponseDto.setFailed("게시글 조회에 실패했습니다.");
+//        }
+//    }
+
+    /*
+     getPosts
+        @Params: 페이지 번호, 페이지 크기
+        return: ResponseDto - 성공 메시지와 페이징된 게시글 목록을 포함
+    */
+    public ResponseDto<PagedResponseDto<PostResponseDto>> getPosts(int page, int size) {
+        PagedResponseDto<PostResponseDto> pagedResponse = null;
+        try {
+            // page와 size 값을 사용해 PageRequest 객체를 생성
+            // : 해당 객체를 통해 DB에 해당 페이지의 Post 목록을 조회
+            // : 결과는 Page<Post> 타입으로 반환
+
+            /*
+                1. PageRequest: Pageable 인터페이스의 구현체
+                                - 특정 페이지의 데이터 조회 요청을 정의하는 객체
+                                - 페이지 번호와 크기(데이터 수)를 기반으로 페이징 요청을 설정
+
+                    EX) PageRequest.of(int page, int size)
+                        : page - 페이지 번호 (0부터 시작)
+                        : size - 한 페이지에 포함할 데이터의 개수
+                        PageRequest.of(2, 10)
+
+                 2. Page<T>: JPA에서 제공하는 인터페이스
+                    , 특정 페이지에 대한 데이터와 페이징 정보를 포함한 객체
+                    - 조회된 데이터 목록뿐만 아니라 페이징과 관련된 메타정보도 함께 제공
+                    - 주요 메서드 -
+                        : getContent() - 현재 페이지 데이터 목록
+                        : getNumber() - 현재 페이지 번호 반환 (0부터 시작)
+                        : getSize() - 한 페이지에 포함된 데이터의 개수 반환
+                        : getTotalPages() - 전체 페이지 수 반환
+                        : getTotalElements() - 전체 데이터 수 반환
+             */
+            Page<Post> postPage = postRepository.findAll(PageRequest.of(page, size));
+
+            List<PostResponseDto> postDtos = postPage.getContent().stream()
+                    .map(PostResponseDto::new)
                     .collect(Collectors.toList());
 
-            if(postResponseDto.isEmpty()) {
-                return ResponseDto.setFailed("등록된 게시글이 없습니다.");
-            }
-            return ResponseDto.setSuccess("게시글을 성공적으로 조회되었습니다.", postResponseDto);
+            pagedResponse = new PagedResponseDto<>(
+                    postDtos,
+                    postPage.getNumber(),   // 요청된 페이지 번호
+                    postPage.getTotalPages(),   // 전체 페이지 수
+                    postPage.getTotalElements() // 전체 요소 수
+            );
         } catch (Exception e) {
-            return ResponseDto.setFailed("게시글 조회에 실패했습니다.");
+            e.printStackTrace();
+            return ResponseDto.setFailed(ResponseMessage.DATABASE_ERROR);
         }
+
+
+        return ResponseDto.setSuccess("게시글 목록 조회 성공", pagedResponse);
     }
-
-
 
 
     // 특정 ID 게시글 조회
